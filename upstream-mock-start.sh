94d6975e85da4fb755667d497633d8a591b5c2d2
Ikenna Nwaiwu <ikenna4u@gmail.com>
2022-07-23 21:21:04 +0100

    DEV-3: rename reference to api-definition. Renaming file

--- upstream-mock-start.sh
+++ upstream-mock-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Using Prism to mock upstream service 
-prism mock --port 3050 api-definitions/compliant-product-catalog-api.oas.yaml > prism-mock.log 2>&1 & 
+prism mock --port 3050 api-definitions/product-catalog-api.oas.yaml > prism-mock.log 2>&1 & 
